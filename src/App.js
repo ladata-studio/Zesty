3a96f726773790831d09906c252b416de31c5229
Ladata Studio <ladata.studio@gmail.com>
2022-12-17 20:48:39 +0100

    Added images

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
 
+import cabbage from './assets/image1.jpeg';
+import mango from './assets/image2.jpeg';
+import fig from './assets/image3.jpeg';
+import gaze from './assets/image4.jpeg';
+import peach from './assets/image5.jpeg';
+import avocado from './assets/image6.jpeg';
+
+const images = [cabbage, mango, fig, gaze, peach, avocado];
+
 const App = () => {
   
   return (
@@ -13,7 +22,7 @@ const App = () => {
         </h2>
       </div>
       <div className='image-container'>
-        <img src='' alt='' />
+        <img src={images[0]} alt='' />
       </div>
     </div>
   );
